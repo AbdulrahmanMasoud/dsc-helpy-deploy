91f8b5b48ddc7e16c63c9267a6166cfd281e7ea8
unknown <shaksia13@gmail.com>
2021-02-24 23:46:07 +0200

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('f_name');
+            $table->string('l_name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('avatar')->default('https://static.thenounproject.com/png/363640-200.png');
+            $table->string('password');
+            $table->enum('gender',['male','female'])->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
